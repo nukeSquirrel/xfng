59d0f7f672e2f002f5c481f3ae89a21dbef77368
nukeSquirrel <nukeSquirrel@gmx.de>
2019-02-23 10:58:53 +0100

    Add Dev Cheat for Creating Ufos via Command Line

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { AppConfig } from './environments/environment';
+import {UfoService} from './model/ufos/ufo-service';
+import {UFO_REPO} from './model/ufos/ufo.repo';
+import {Ufo} from './model/ufos/Ufo.entity';
 
 if (AppConfig.production) {
   enableProdMode();
+} else {
+  console.log('Dev Cheats Enabled!');
+  (<any> window).createUfo = () => { UFO_REPO.add(new Ufo()); };
 }
 
 platformBrowserDynamic()
